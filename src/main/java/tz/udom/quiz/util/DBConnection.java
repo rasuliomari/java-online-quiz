4f5a1566ce55e31f7bf9f7631115802999a9ad5b
rasuliomari <rasuli707046@gmail.com>
2026-09-01 01:26:11 +0300

    This is the innitial setup of the project of online quize

--- src/main/java/tz/udom/quiz/util/DBConnection.java
+++ src/main/java/tz/udom/quiz/util/DBConnection.java
@@ -6,16 +6,11 @@ import java.sql.SQLException;
 
 public class DBConnection {
 
-    private static final String URL =
-            "jdbc:postgresql://localhost:5432/udom_quiz_db";
-
+    private static final String URL ="jdbc:postgresql://localhost:5432/udom_quiz_db";
     private static final String USER = "admin";
-
     private static final String PASSWORD = "admin";
-
     public static Connection getConnection()
             throws SQLException {
-
         return DriverManager.getConnection(URL, USER, PASSWORD);
     }
 }
--- src/main/java/tz/udom/quiz/util/DatabaseTest.java
+++ src/main/java/tz/udom/quiz/util/DatabaseTest.java
@@ -7,22 +7,13 @@ public class DatabaseTest {
     public static void main(String[] args) {
 
         try {
-
-            Connection connection =
-                    DBConnection.getConnection();
-
-            System.out.println(
-                    "PostgreSQL connection successful!"
-            );
-
+            Connection connection = DBConnection.getConnection();
+            System.out.println("PostgreSQL connection successful!");
             connection.close();
-
         } catch (Exception e) {
-
             System.out.println(
                     "Database connection failed!"
             );
-
             e.printStackTrace();
         }
     }
